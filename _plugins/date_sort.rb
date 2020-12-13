e92ff903068814673addc80f577363ca4b60c2ff
Nicuvëo <crucuny@gmail.com>
2020-12-13 21:12:20 +0000

    Rewrote main page, improved small screen css.

--- _plugins/date_sort.rb
+++ _plugins/date_sort.rb
@@ -5,8 +5,16 @@ module Jekyll
 
     def generate(site)
       site.data["yearly"] = {}
-      site.posts.docs.each.group_by { |post| post.date.year }.each do |y, l|
-        site.data["yearly"][y] = l
+      site.categories.each do |lang, pages|
+        site.data["yearly"][lang] = []
+        pages.each.group_by { |p| p.date.year }.each do |y, l|
+          object = {
+            "year"  => y,
+            "count" => l.size,
+            "pages" => l
+          }
+          site.data["yearly"][lang] += [object]
+        end
       end
     end
   end
